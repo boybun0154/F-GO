3e6e964a947c8e4f98e98a5566d33cf51b3653a9
boybun0154 <huyhoang201555@gmail.com>
2023-03-23 14:02:04 +0700

    verify pages

--- src/main/java/UserControl/EditCustomerControl.java
+++ src/main/java/UserControl/EditCustomerControl.java
@@ -21,12 +21,7 @@ import javax.servlet.http.Part;
  *
  * @author ADMIN
  */
-@MultipartConfig(
-        location = "D:\\Intellij\\F-GO\\src\\main\\webapp\\images",
-        fileSizeThreshold = 1024 * 1024 * 10,
-        maxFileSize = 1024 * 1024 * 50,
-        maxRequestSize = 1024 * 1024 * 100
-)
+
 @WebServlet(name = "EditCustomerControl", urlPatterns = {"/edit_customer"})
 public class EditCustomerControl extends HttpServlet {
 
