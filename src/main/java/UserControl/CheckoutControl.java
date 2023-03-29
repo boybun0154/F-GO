2879d43e0ef5ebf052165da3f44e76bda553aa66
Your Name <ltk23102002@gmail.com>
2023-03-29 09:21:09 +0700

    vnpay

--- src/main/java/UserControl/CheckoutControl.java
+++ src/main/java/UserControl/CheckoutControl.java
@@ -122,7 +122,87 @@ public class CheckoutControl extends HttpServlet {
                 request.getRequestDispatcher("error.jsp").forward(request, response);
             }
         }
+        if (payment_method.equals("vnpay")) {
+            Order order = checkoutdao.getOrder();
 
+            String vnp_Version = "2.1.0";
+            String vnp_Command = "pay";
+            String vnp_OrderInfo = "Thanh toan don hang " + order.getOrderId();
+            String orderType = "thanh toan thue xe";
+            String vnp_TxnRef = Config.getRandomNumber(8);
+            String vnp_IpAddr = Config.getIpAddress(request);
+            String vnp_TmnCode = Config.vnp_TmnCode;
+
+            int amount = (order.getTotalMoney() * 100) / 2;
+            Map<String, String> vnp_Params = new HashMap<>();
+            vnp_Params.put("vnp_Version", vnp_Version);
+            vnp_Params.put("vnp_Command", vnp_Command);
+            vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+            vnp_Params.put("vnp_Amount", String.valueOf(amount));
+            vnp_Params.put("vnp_CurrCode", "VND");
+            String bank_code = "";
+            if (bank_code != null && !bank_code.isEmpty()) {
+                vnp_Params.put("vnp_BankCode", bank_code);
+            }
+            vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+            vnp_Params.put("vnp_OrderInfo", vnp_OrderInfo);
+            vnp_Params.put("vnp_OrderType", orderType);
+
+            String locate = "vn";
+            if (locate != null && !locate.isEmpty()) {
+                vnp_Params.put("vnp_Locale", locate);
+            } else {
+                vnp_Params.put("vnp_Locale", "vn");
+            }
+            vnp_Params.put("vnp_ReturnUrl", Config.vnp_Returnurl);
+            vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+            Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+            String vnp_CreateDate = formatter.format(cld.getTime());
+
+            vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+            cld.add(Calendar.MINUTE, 15);
+            String vnp_ExpireDate = formatter.format(cld.getTime());
+            //Add Params of 2.0.1 Version
+            vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+            //Build data to hash and querystring
+            List fieldNames = new ArrayList(vnp_Params.keySet());
+            Collections.sort(fieldNames);
+            StringBuilder hashData = new StringBuilder();
+            StringBuilder query = new StringBuilder();
+            Iterator itr = fieldNames.iterator();
+            while (itr.hasNext()) {
+                String fieldName = (String) itr.next();
+                String fieldValue = (String) vnp_Params.get(fieldName);
+                if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                    //Build hash data
+                    hashData.append(fieldName);
+                    hashData.append('=');
+                    hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                    //Build query
+                    query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                    query.append('=');
+                    query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                    if (itr.hasNext()) {
+                        query.append('&');
+                        hashData.append('&');
+                    }
+                }
+            }
+            String queryUrl = query.toString();
+            String vnp_SecureHash = Config.hmacSHA512(Config.vnp_HashSecret, hashData.toString());
+            queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+            String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+            com.google.gson.JsonObject job = new JsonObject();
+            job.addProperty("code", "00");
+            job.addProperty("message", "success");
+            job.addProperty("data", paymentUrl);
+            Gson gson = new Gson();
+            response.getWriter().write(gson.toJson(job));
+            response.sendRedirect(paymentUrl);
+        }
+    
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
